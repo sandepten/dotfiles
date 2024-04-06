e74e2c9ed1e510a3858c53586af1fd2f1728e46a
Sandeep Kumar <sandepten@gmail.com>
2024-04-06 11:27:43 +0530

    Add: more files

--- dot_zsh/alias.zsh
+++ dot_zsh/alias.zsh
@@ -31,3 +31,10 @@ p() { # detect package manager and run it
 }
 
 # Others
+nah() {
+    git reset --hard
+    git clean -df
+    if [ -d ".git/rebase-apply" ] || [ -d ".git/rebase-merge" ]; then
+        git rebase --abort
+    fi
+}

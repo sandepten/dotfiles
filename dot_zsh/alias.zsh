af8348208a2042c95721d44c5f2ae6c02ada2abb
Sandeep Kumar <sandepten@gmail.com>
2024-06-17 11:12:54 +0530

    sync office laptop

--- dot_zsh/alias.zsh
+++ dot_zsh/alias.zsh
@@ -6,11 +6,19 @@ alias prisma="npx prisma"
 alias cat=bat # bat is a cat clone with syntax highlighting and Git integration
 
 # exa replace ls
-alias ls="exa"
-alias ll="exa -alh"
-alias la="exa -la"
-alias l="exa -l --git"
 alias tree="exa --tree --level=3"
+alias c='clear'                                                        # clear terminal
+alias l='eza -lh  --icons=auto'                                        # long list
+alias ls='eza -1   --icons=auto'                                       # short list
+alias ll='eza -lha --icons=auto --sort=name --group-directories-first' # long list all
+alias ld='eza -lhD --icons=auto'                                       # long list dirs
+alias lt='eza --icons=auto --tree'                                     # list folder as tree
+alias un='$aurhelper -Rns'                                             # uninstall package
+alias up='$aurhelper -Syu'                                             # update system/package/aur
+alias pl='$aurhelper -Qs'                                              # list installed package
+alias pa='$aurhelper -Ss'                                              # list availabe package
+alias pc='$aurhelper -Sc'                                              # remove unused cache
+alias po='$aurhelper -Qtdq | $aurhelper -Rns -'                        # remove unused packages, also try > $aurhelper -Qqd | $aurhelper -Rsu --print -
 
 # JS Ecosystem
 p() { # detect package manager and run it
@@ -35,3 +43,13 @@ nah() {
     git rebase --abort
   fi
 }
+
+# Handy change dir shortcuts
+alias ..='cd ..'
+alias ...='cd ../..'
+alias .3='cd ../../..'
+alias .4='cd ../../../..'
+alias .5='cd ../../../../..'
+
+# Always mkdir a path (this doesn't inhibit functionality to make a single dir)
+alias mkdir='mkdir -p'

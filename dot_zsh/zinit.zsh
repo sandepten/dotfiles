897597b31826cc2c96700baaef1f7ab572db7e02
Sandeep Kumar <sandepten@gmail.com>
2025-03-06 10:06:51 +0530

    move zinit config to new file

---
+++ dot_zsh/zinit.zsh
@@ -0,0 +1,32 @@
+#? Zinit
+# Directory to store zinit and plugins
+ZINIT_HOME="${XDG_DATA_HOME:-$HOME/.local/share}/zinit/zinit.git"
+
+# Download Zinit, if not already present
+if [[ ! -d $ZINIT_HOME ]]; then
+  mkdir -p "$(dirname $ZINIT_HOME)"
+  git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
+fi
+
+source ~/.zsh/catppuccin_mocha-zsh-syntax-highlighting.zsh
+
+# Load Zinit
+source "$ZINIT_HOME/zinit.zsh"
+
+# Load plugins
+zinit light zsh-users/zsh-autosuggestions
+zinit light zsh-users/zsh-completions
+zinit light zsh-users/zsh-syntax-highlighting
+zinit load atuinsh/atuin
+zinit light Aloxaf/fzf-tab
+
+# Add in snippets
+zinit snippet OMZP::git
+zinit snippet OMZP::sudo
+zinit snippet OMZP::command-not-found
+zinit snippet OMZP::vscode
+
+# Load completions
+autoload -U compinit && compinit
+
+zinit cdreplay -q

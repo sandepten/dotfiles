86787cc88b6c1978829d44123e3ee038937dad73
Sandeep Kumar <sandepten@gmail.com>
2025-08-23 18:59:21 +0530

    zsh: move zsh config to xdg config

--- dot_config/zsh/zsh/alias.zsh
+++ dot_config/zsh/zsh/alias.zsh
@@ -137,3 +137,12 @@ alias f='open -a Finder '
 
 # funny sudo
 alias please='sudo'
+
+# tmux
+# Bind the widget to Ctrl+P
+# bindkey '^P' _fzf_tmux_switcher
+bindkey -s ^p "tmux-sessionizer\n"
+bindkey -s '\eh' "tmux-sessionizer -s 0\n"
+bindkey -s '\et' "tmux-sessionizer -s 1\n"
+bindkey -s '\en' "tmux-sessionizer -s 2\n"
+bindkey -s '\es' "tmux-sessionizer -s 3\n"

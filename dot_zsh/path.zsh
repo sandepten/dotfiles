71d02c64cc8bbfe7e9acbd530d7ba241702fd36e
Sandeep Kumar <sandepten@gmail.com>
2025-08-23 19:17:17 +0530

    Revert "zsh: move envs to zshenv"
    This reverts commit 2686ce95a6b451797c7f80a0835ee6cb2c41cae8.

---
+++ dot_zsh/path.zsh
@@ -0,0 +1,39 @@
+#! These are the path requirements for the system
+
+# Go
+export PATH=$PATH:~/go/bin
+export PATH=$PATH:/usr/local/go/bin
+export PATH="$PATH:~/.local/bin"
+
+# pnpm
+export PNPM_HOME="$HOME/.local/share/pnpm"
+case ":$PATH:" in
+*":$PNPM_HOME:"*) ;;
+*) export PATH="$PNPM_HOME:$PATH" ;;
+esac
+# pnpm end
+
+# pipx
+if ! [[ "$PATH" =~ "$HOME/.local/bin:$HOME/bin:" ]]; then
+  PATH="$HOME/.local/bin:$HOME/bin:$PATH"
+fi
+export PATH
+
+if [ "$(uname)" = "Darwin" ]; then
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
+
+## nvim as default editor
+if [ -x "$(command -v nvim)" ]; then
+  export EDITOR=nvim
+fi
+
+# Shell integrations
+# source <(fzf --zsh)
+eval "$(zoxide init --cmd cd zsh)"
+eval "$(atuin init zsh)"
+eval $(thefuck --alias)
+eval "$(fnm env --use-on-cd --shell zsh)"
+
+# theo video node cache speed up
+export NODE_COMPILE_CACHE=~/.cache/nodejs-compile-cache

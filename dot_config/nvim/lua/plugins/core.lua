6198511079c868e389d93ccffb7ae973bd002596
Sandeep Kumar <sandepten@gmail.com>
2025-08-29 11:56:56 +0530

    nvim: lazy sync

--- dot_config/nvim/lua/plugins/core.lua
+++ dot_config/nvim/lua/plugins/core.lua
@@ -8,4 +8,13 @@ return {
       -- colorscheme = "rose-pine",
     },
   },
+  {
+    "akinsho/bufferline.nvim",
+    init = function()
+      local bufline = require("catppuccin.groups.integrations.bufferline")
+      function bufline.get()
+        return bufline.get_theme()
+      end
+    end,
+  },
 }

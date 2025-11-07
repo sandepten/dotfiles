c37312db035b75575741c8fa32b2cb2e54e7e651
Sandeep Kumar <sandepten@gmail.com>
2025-11-07 10:52:01 +0530

    zsh: update zinit initial install

--- dot_zsh/zinit.zsh
+++ dot_zsh/zinit.zsh
@@ -1,8 +1,8 @@
 #— Define where Zinit lives
-ZINIT_HOME="${XDG_DATA_HOME:-$HOME/.local/share}/zinit/zinit.git"
-[[ ! -d $ZINIT_HOME ]] && mkdir -p "$(dirname $ZINIT_HOME)" \
-  && git clone https://github.com/zinit-zsh/zinit.git "$ZINIT_HOME"
-source "$ZINIT_HOME/zinit.zsh"
+ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
+[ ! -d $ZINIT_HOME ] && mkdir -p "$(dirname $ZINIT_HOME)"
+[ ! -d $ZINIT_HOME/.git ] && git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
+source "${ZINIT_HOME}/zinit.zsh"
 
 #— Initialize completions
 autoload -U compinit && compinit

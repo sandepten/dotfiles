5ba31c4d29ba2dc07e6bba756c8c1d9c4bb80982
Sandeep Kumar <sandepten@gmail.com>
2026-04-08 15:28:08 +0530

    nvim: fix catppuccin workaround

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -5,3 +5,21 @@ vim.api.nvim_create_autocmd({ "BufNewFile", "BufReadPost" }, {
     vim.diagnostic.enable(false, { bufnr = e.buf })
   end,
 })
+
+-- Workaround: catppuccin compiled cache doesn't apply opts correctly on Neovim 0.12.
+vim.api.nvim_create_autocmd("ColorScheme", {
+  group = vim.api.nvim_create_augroup("catppuccin_recompile", { clear = true }),
+  pattern = "catppuccin*",
+  once = true,
+  callback = function()
+    vim.schedule(function()
+      for name, _ in pairs(package.loaded) do
+        if name:match("^catppuccin%.") then
+          package.loaded[name] = nil
+        end
+      end
+      require("catppuccin").compile()
+      vim.cmd.colorscheme("catppuccin")
+    end)
+  end,
+})
